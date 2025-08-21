839697a8124d19c4859867d8305da9527a7367f3
chibakoudai <s21a5071pg@s.chibakoudai.jp>
2025-08-21 18:59:12 +0900

    test

--- src/contract/config.jsx
+++ src/contract/config.jsx
@@ -8,7 +8,8 @@ const rpc = "https://rpc-amoy.polygon.technology/"; // (required for Ethereum) J
 //const token_address = "0x1ceA098E584e46c7659f8460d3c13Cec2D0B22F4";
 
 //const quiz_address = "0x681913855E68BBF88962A40E4f3f48cB78fc9603";
-const quiz_address = "0xAb3Ec4a039fb6aBb66Cf00460d27839a9C196B94";
+//const quiz_address = "0xAb3Ec4a039fb6aBb66Cf00460d27839a9C196B94";//応用数学一回目
+const quiz_address = "0x5d12efccbd81c60c80e5e2caffa480f2cf80a813"
 const token_address = "0x021e416bb6bfA1e76Aa4E280828b1d55F2d5f2F0";
 
 export { chainId, rpc, quiz_address, token_address };
